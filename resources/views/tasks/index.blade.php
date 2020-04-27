840006347bff05c0566f18e5a7893ce1af356b60
yumi.nagahara <y.git.783@gmail.com>
2020-04-27 07:59:41 +0000

    CRUD tasks

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!--以下コンテンツ-->
+
+    <h1>タスク一覧</h1>
+    
+    @if (count($tasks) >0 )
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タスク</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($tasks as $task)
+                <tr>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        @endif
+        
+        {!! link_to_route('tasks.create','新規タスクの投稿',[],['class' => 'btn btn-primary']) !!}
+@endsection
