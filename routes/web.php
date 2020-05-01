eea222d6f6104499c54d95360e9c0201f9122086
yumi.nagahara <y.git.783@gmail.com>
2020-05-01 05:54:33 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
 
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 /*
 // CRUD
 // メッセージの個別詳細ページ表示
